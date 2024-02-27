b0d4618cd0a57614094e8976cfe0299ecd020b72
Andrea Peretti <andrea@andxet.dev>
2024-02-27 18:52:21 +0100

    Added quest logic

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
-@export var enable_on_debug: Array[Node]
-@export var disable_on_debug: Array[Node]
-@export var enable_on_release: Array[Node]
-@export var disable_on_release: Array[Node]
+@onready var virtual_joysticks = $UI/VirtualJoysticks
 
-@onready var virtual_joysticks = $"../UI/VirtualJoysticks"
+@onready var idol = $PickupableIdol
+@onready var door_side = $door_side
+@onready var door_front = $door_front
+@onready var lever = $"front door lever"
+@onready var traps = $Traps
+@onready var close_door_area = $"Close door area"
+
+@export var quest_label: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,8 +25,46 @@ func _ready():
 		print("Debug build")
 	elif OS.has_feature("release"):
 		print("Release build")
+	assert(virtual_joysticks)
+	assert(idol)
+	assert(door_side)
+	assert(door_front)
+	assert(lever)
+	assert(traps)
+	assert(close_door_area)
+	assert(quest_label)
+	
+	quest_label.text = ""
+	quest()
 
+func quest():	
+	print("Starting game!")
+	quest_label.text = "Find the idol and run!"
+	await get_tree().create_timer(5).timeout
+	
+	quest_label.text = "Find a way to get in"
+	await lever.on_pull_down
+	door_front.locked = false
+	door_front.open()
+	
+	quest_label.text = "The front door is open!"
+	await get_tree().create_timer(5).timeout
+	
+	quest_label.text = "Get in and take the idol!"
+	await idol.on_picked_up
+	quest_label.text = "The idol is vanishing! Get back!"
+	
+	await idol.on_end_pick_animation
+	quest_label.text = "It's a trap! Get out! Get out!"
+	
+	#activate traps
+		
+	await close_door_area.area_entered
+	door_front.close()
+	door_front.locked = true
+	
+	# End quest
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _on_side_door_lever_on_pull():
+	door_side.locked = false
+	door_side.open()
