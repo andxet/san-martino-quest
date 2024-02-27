5fd3811d8393628cb3f58a0cf02af0dac8eef65d
Andrea Peretti <andrea@andxet.dev>
2024-02-27 18:50:29 +0100

    Updated doors

--- models/door_01/door.gd
+++ models/door_01/door.gd
@@ -2,35 +2,38 @@ extends Node3D
 
 @onready var animation_player = $AnimationPlayer
 
-@export var start_closed : bool = true
-var is_open : bool
+@export var locked: bool = true
+
+var is_open : bool = false
 
 signal on_closed
 signal on_opened
+signal on_locked
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if(start_closed):
-		animation_player.seek(0)
-	else:
-		animation_player.seek(animation_player.current_animation_length)
-	is_open = !start_closed
+	if locked:
+		animation_player.play("door/RESET")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func open():
-	animation_player.speed_scale = 1
-	animation_player.play()
+	if locked:
+		on_locked.emit()
+		return
+	$AnimationPlayer.play("door/open")
+	is_open = true
 	
 func close():
-	animation_player.speed_scale = -1
-	animation_player.play()
+	$AnimationPlayer.play_backwards("door/open")
+	is_open = false
 
 
 func _on_interaction_area_on_used():
+	print("Action used!! %s", is_open)
 	if(is_open):
 		close()
 	else:
