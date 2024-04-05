b522e9b174faa924a25428e5e0bee704b5c439cc
Andrea Peretti <andrea@andxet.dev>
2024-04-05 14:36:59 +0200

    Changed idol pickup animation

--- scripts/pickupable_idol.gd
+++ scripts/pickupable_idol.gd
@@ -32,21 +32,23 @@ func pickup_visual():
 	
 	on_picked_up.emit()
 	
+	if tween_root:
+		rotation_tween.kill()
+		
+		rotation_tween = get_tree().create_tween()
+		rotation_tween.set_loops()
+		rotation_tween.tween_property(tween_root, "rotation:y", deg_to_rad(360), 0.3).as_relative()		
+		
 	if light:
 		var light_tween = get_tree().create_tween()
 		light_tween.tween_property(light, "light_energy", 15, 3)
 		await light_tween.finished
 		print("finished")
-	rotation_tween.kill()
 	
 	if tween_root:
 		var size_tween = get_tree().create_tween()
 		size_tween.tween_property(tween_root, "scale", Vector3(0,5,0), 1)
-		
-		rotation_tween = get_tree().create_tween()
-		rotation_tween.set_loops()
-		rotation_tween.tween_property(tween_root, "rotation:y", deg_to_rad(360), 0.3).as_relative()
-		
+			
 		await size_tween.finished
 		
 	on_end_pick_animation.emit()
